c54ddc542707c742ae1a2eaea60fcc800037a533
esam23 <erik.steven17@gmail.com>
2019-06-09 18:09:35 -0500

    Post Formulario

--- client/src/app/components/post-form/post-form.component.ts
+++ client/src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { PostService } from './../../services/post.service';
+import { Post } from './../../models/Post';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostFormComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('class') classes = 'row';
+
+  post: Post = {
+    id_post: 0,
+    titulo: '',
+    comentario: '',
+    cod_equipo:'', 
+    fecha_post: new Date()
+};
+
+edit: boolean = false;
+
+  constructor( private postService : PostService, private router: Router, private  activedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    const params = this.activedRoute.snapshot.params;
+    console.log(params)
+    if (params.id){
+    
+      this.postService.getPost(params.id)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.post = res;
+          this.edit = true;
+        },
+        err => console.error(err)
+      ) 
+    }
+  }
+
+  saveNewPlayer(){
+    delete this.post.fecha_post;
+    delete this.post.id_post;
+
+    this.postService.savePost(this.post)
+    .subscribe(
+      res => {
+      console.log(res);
+      this.router.navigate(['/posts']);
+      },
+      err => console.error(err)
+    )
+  }
+
+  updatePlayer(){
+    delete this.post.fecha_post;
+    this.postService.updatePost(this.post.id_post,this.post)
+    .subscribe(
+      res =>{
+        console.log(res);
+        this.router.navigate(['/posts']);
+      },
+      err => console.error(err)
+    )
+    
   }
 
 }
