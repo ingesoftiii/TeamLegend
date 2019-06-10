0ef273bf052ba7f1ec8e2e2b6f4ddac2af2e8fa5
esam23 <erik.steven17@gmail.com>
2019-06-10 04:10:59 -0500

    Login Logica

--- client/src/app/components/administrador/administrador.component.ts
+++ client/src/app/components/administrador/administrador.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayersService } from '../../services/players.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-administrador',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministradorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private playersService: PlayersService,  private router: Router) { }
 
   ngOnInit() {
+
+  }
+
+  
+  loginUser(e){  
+    
+    
+    e.preventDefault();
+    
+    console.log(e);
+
+    var username = e.target.elements[0].value;
+    var password = e.target.elements[1].value;
+    
+        
+     if (username == 'admin' && password == 'admin'){
+          this.router.navigate(['players']);
+     }
   }
 
 }
